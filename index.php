1cee9ffae2f197b5c8c7f95c1fe118bb3093d05c
will14n <willianf4m@gmail.com>
2017-08-22 01:13:39 -0300

    make it better

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-$connect = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
-print_r($connect);
+// $connect = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
+
+  # get the mongo db name out of the env
+  $mongo_url = parse_url(getenv("MONGO_URL"));
+  $dbname = str_replace("/", "", $mongo_url["path"]);
+
+  # connect
+  $m   = new Mongo(getenv("MONGO_URL"));
+  $db  = $m->$dbname;
+  $col = $db->access;
+
+  # insert a document
+  $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+  $col->insert($visit);
+
+  # print all existing documents
+  $data = $col->find();
+  foreach($data as $visit) {
+    echo "<li>" . $visit["ip"] . "</li>";
+  }
+
+  # disconnect
+  $m->close();
+
 // header("location: ./controller/index.php");
