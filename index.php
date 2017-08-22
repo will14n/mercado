e662209aacfe39a240c23c11c7faf15fbcfb623e
will14n <willianf4m@gmail.com>
2017-08-22 01:26:44 -0300

    make it better

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php
-ini_set('display_errors',1);
-error_reporting(E_ALL);
-// $connect = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
+// ini_set('display_errors',1);
+// error_reporting(E_ALL);
+// // $connect = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
 
-  # get the mongo db name out of the env
-  $mongo_url = parse_url(getenv("MONGODB_URI"));
+//   # get the mongo db name out of the env
+//   $mongo_url = parse_url(getenv("MONGODB_URI"));
 
-  $dbname = str_replace("/", "", $mongo_url["path"]);
+//   $dbname = str_replace("/", "", $mongo_url["path"]);
 
-  # connect
-  $m = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
-  // $m   = new Mongo(getenv("MONGODB_URI"));
-  $db  = $m->$dbname;
-  $col = $db->access;
+//   # connect
+//   $m = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
+//   // $m   = new Mongo(getenv("MONGODB_URI"));
+//   $db  = $m->$dbname;
+//   $col = $db->access;
 
-  # insert a document
-  $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
-  $col->insert($visit);
+//   # insert a document
+//   $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+//   $col->insert($visit);
 
-  # print all existing documents
-  $data = $col->find();
-  foreach($data as $visit) {
-    echo "<li>" . $visit["ip"] . "</li>";
-  }
+//   # print all existing documents
+//   $data = $col->find();
+//   foreach($data as $visit) {
+//     echo "<li>" . $visit["ip"] . "</li>";
+//   }
 
-  # disconnect
-  $m->close();
+//   # disconnect
+//   $m->close();
 
-// header("location: ./controller/index.php");
+header("location: ./controller/index.php");
