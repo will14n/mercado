d8f6c0641a930ac000fc09a41944943bbc7ebb90
will14n <willianf4m@gmail.com>
2017-08-22 00:57:52 -0300

    make it better

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+$connect = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
+print_r($connect);
 // header("location: ./controller/index.php");
