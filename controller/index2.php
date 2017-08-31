17d6da4fbbf09acf953961b8b84375243ef87685
will14n <willianf4m@gmail.com>
2017-08-31 03:51:51 -0300

    removido código e acrescentado debug na classe teste

--- controller/index2.php
+++ controller/index2.php
@@ -2,11 +2,9 @@
 session_start();
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-include_once '../classes/conecta.php';
+include_once '../classes/conectaHeroku.php';
 require_once("../lib/raelgc/view/Template.php");
 use raelgc\view\Template;
-
-$tpl = new Template("../pages/nav.html");
 
 $tpl = new Template("../pages/dados.html");
 $tpl->addFile("DADOS", "../pages/cadastros.html");
@@ -19,8 +17,10 @@ $cadastrar->setServidor('localhost');
 $cadastrar->setUserCon('root');
 $cadastrar->setPwdCon('root');
 $cadastrar->setBaseCon('admin');
-$cadastrar->setCon($con);
 $cadastrar->setBaseCons('mercado.promocao');
+$cadastrar->conecta();
+print_r($cadastrar);
 $cadastrar->insere();
+print_r($cadastrar);
 
 ?>
