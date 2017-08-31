914f8d7f481a7429a3c47a9cf4ee33c047383fc1
will14n <willianf4m@gmail.com>
2017-08-31 04:09:46 -0300

    testando conexao

--- classes/conectaHeroku.php
+++ classes/conectaHeroku.php
@@ -67,8 +67,29 @@ class Conectar{
     }
 
     function insere(){
-        $connect = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
+        /*$connect = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
         var_dump($connect);
-        return "OK!!!";
+        return "OK!!!";*/
+         # get the mongo db name out of the env
+          $mongo_url = parse_url(getenv("MONGO_URL"));
+          $dbname = str_replace("/", "", $mongo_url["path"]);
+
+          # connect
+          $m   = new Mongo(getenv("MONGO_URL"));
+          $db  = $m->$dbname;
+          $col = $db->access;
+          print_r($col);
+          # insert a document
+          $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+          $col->insert($visit);
+
+          # print all existing documents
+          $data = $col->find();
+          foreach($data as $visit) {
+            echo "<li>" . $visit["ip"] . "</li>";
+          }
+
+          # disconnect
+          $m->close();
     }
 }
