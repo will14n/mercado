ef5d521b2a12576601980dd74283063d4275a322
will14n <willianf4m@gmail.com>
2017-09-06 12:50:36 -0300

    debug banco

--- index.php
+++ index.php
@@ -20,12 +20,47 @@ $cadastrar->setBaseCons('mercado.filiais');
 $cadastrar->insere();*/
 
 $bulk = new MongoDB\Driver\BulkWrite(['ordered' => true]);
-$bulk->insert(['_id' => 1, 'x' => 1]);
-$bulk->insert(['_id' => 2, 'x' => 2]);
-$bulk->update(['x' => 2], ['$set' => ['x' => 1]]);
-$bulk->insert(['_id' => 3, 'x' => 3]);
-$bulk->delete(['x' => 1]);
+$bulk->delete([]);
+$bulk->insert(['_id' => 1]);
+$bulk->insert(['_id' => 2]);
+$bulk->insert(['_id' => 3, 'hello' => 'world']);
+$bulk->update(['_id' => 3], ['$set' => ['hello' => 'earth']]);
+$bulk->insert(['_id' => 4, 'hello' => 'pluto']);
+$bulk->update(['_id' => 4], ['$set' => ['hello' => 'moon']]);
+$bulk->insert(['_id' => 3]);
+$bulk->insert(['_id' => 4]);
+$bulk->insert(['_id' => 5]);
 
-print_r($bulk);
-// print_r($cadastrar);
+$manager = new MongoDB\Driver\Manager('mongodb://localhost:27017');
+$writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+
+try {
+    $result = $manager->executeBulkWrite('db.collection', $bulk, $writeConcern);
+} catch (MongoDB\Driver\Exception\BulkWriteException $e) {
+    $result = $e->getWriteResult();
+
+    // Check if the write concern could not be fulfilled
+    if ($writeConcernError = $result->getWriteConcernError()) {
+        printf("%s (%d): %s\n",
+            $writeConcernError->getMessage(),
+            $writeConcernError->getCode(),
+            var_export($writeConcernError->getInfo(), true)
+        );
+    }
+
+    // Check if any write operations did not complete at all
+    foreach ($result->getWriteErrors() as $writeError) {
+        printf("Operation#%d: %s (%d)\n",
+            $writeError->getIndex(),
+            $writeError->getMessage(),
+            $writeError->getCode()
+        );
+    }
+} catch (MongoDB\Driver\Exception\Exception $e) {
+    printf("Other error: %s\n", $e->getMessage());
+    exit;
+}
+
+printf("Inserted %d document(s)\n", $result->getInsertedCount());
+printf("Updated  %d document(s)\n", $result->getModifiedCount());// print_r($cadastrar);
 // header("location: ./controller/index.php");
