0b6c8dc347e5d4725e3004a3f7226c154a64ea25
will14n <willianf4m@gmail.com>
2017-09-06 20:27:16 -0300

    removido debug banco

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-include_once './classes/conectaHeroku.php';
 
-$con = [
-                    'cod_filial' => 'filialCodigo',
-                    'nome' => 'filialNome',
-                    'endereco' => 'filialEndereco',
-                    'observacao' => 'filialObservacao'
-                ];
-
-$cadastrar = new Conectar();
-$cadastrar->setServidor('localhost');
-$cadastrar->setUserCon('root');
-$cadastrar->setPwdCon('root');
-$cadastrar->setBaseCon('admin');
-$cadastrar->setCon($con);
-$cadastrar->setBaseCons('mercado.filiais');
-$cadastrar->insere();
-
-print_r($cadastrar);
-// header("location: ./controller/index.php");
+header("location: ./controller/index.php");
