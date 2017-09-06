30fbf12f478cfcefe624904fbcd039f8f5817831
will14n <willianf4m@gmail.com>
2017-09-06 11:42:11 -0300

    debug banco

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-header("location: ./controller/index.php");
+
+$con = [
+                    'cod_filial' => $_POST['filialCodigo'],
+                    'nome' => $_POST['filialNome'],
+                    'endereco' => $_POST['filialEndereco'],
+                    'observacao' => $_POST['filialObservacao']
+                ];
+
+$connect = new \MongoDB\Driver\Manager("mongodb://admin:admin@ds023523.mlab.com:23523/mercado");
+$bulk = new MongoDB\Driver\BulkWrite;
+$doc = $con;
+$bulk->insert($doc);
+$connect2->executeBulkWrite('admin', $bulk); 
+
+print_r($connect2);
+// header("location: ./controller/index.php");
