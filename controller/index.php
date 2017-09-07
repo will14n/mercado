e298da3f190d92e8e0b1b6d2a13095adb2048cc0
will14n <willianf4m@gmail.com>
2017-09-07 01:31:42 -0300

    debug

--- controller/index.php
+++ controller/index.php
@@ -210,7 +210,7 @@ else if($_GET['page'] == 'login') {
 				$cadastrar->insere(); 
 				$_SESSION['usuario'] = $_POST['usr'];
 				// header('location: ./index.php?page=cadastro&tipo=login');
-				$tpl->addFile("DADOS", "../pages/cadastrado.html");
+				// $tpl->addFile("DADOS", "../pages/cadastrado.html");
 				$tpl->NOME = $con['login'];
 				$tpl->block("BLOCK_CADASTRO");
 			}
