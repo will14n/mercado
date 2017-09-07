f0db595f5dece6dbe1fefa119892133d61cfedb8
will14n <willianf4m@gmail.com>
2017-09-07 01:28:24 -0300

    debug

--- controller/index.php
+++ controller/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-// ini_set('display_errors',1);
-// error_reporting(E_ALL);
+ini_set('display_errors',1);
+error_reporting(E_ALL);
 // include_once '../classes/conecta.php'; #CLASSE DE CONEXAO LOCAL
 include_once '../classes/conectaHeroku.php'; #CLASSE DE CONEXAO HEROKU
 require_once("../lib/raelgc/view/Template.php");
