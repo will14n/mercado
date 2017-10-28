5958e949a99a4336fc1b6e1f4fe6764d311a95aa
will14n <willianf4m@gmail.com>
2017-10-28 12:08:43 -0200

    debug

--- controller/index.php
+++ controller/index.php
@@ -240,6 +240,7 @@ else if($_GET['page'] == 'login') {
 				foreach ($teste->conecta() as $p) {
 
 					if(md5($_POST['pwd']) === $p->senha) {
+						print_r($p);exit;
 						$_SESSION['autentica'] = "true";
 						$_SESSION['usuario'] = $_POST['usr'];
 						header('location: ./index.php?page=login');
