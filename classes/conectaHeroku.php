1f9fe9d147b760e618ba40aba79b0aaaaf18de9e
will14n <willianf4m@gmail.com>
2017-10-28 11:07:20 -0200

    Conecxao

--- classes/conectaHeroku.php
+++ classes/conectaHeroku.php
@@ -72,7 +72,7 @@ class Conectar{
         $bulk = new MongoDB\Driver\BulkWrite;
         $doc = $this->con;
         $bulk->insert($doc);
-        $connect->executeBulkWrite($this->baseCon, $bulk);
+        $connect->executeBulkWrite($this->baseCons, $bulk);
         return "OK!!!";   
     }
 }
